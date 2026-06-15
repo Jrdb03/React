91800aeb50c93670131b6b32f0b79a15e2203ad7
Jrdb03 <jregaladcv@alumnos.unex.es>
2026-06-15 14:01:46 +0200

    feat: initialize Gifs app with basic structure and components

---
+++ 02-gifs-app/src/GifsApp.tsx
@@ -0,0 +1,40 @@
+import { mockGifs } from "./mock-data/gifs.mock";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SearchBar } from "./shared/components/SearchBar";
+
+export const GifsApp = () => {
+  return (
+    <>
+        {/* Header */}
+        <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el gif"/>
+
+        {/* Search */}
+        <SearchBar />
+
+        {/* Búsquedas previas */}
+        <div className="previous-searches">
+            <h2>Búsquedas previas</h2>
+            <ul className="previous-searches-list">
+                <li>Goku</li>
+                <li>Saitama</li>
+                <li>Elden Ring</li>
+            </ul>
+        </div>
+
+        {/* Gifs */}
+        <div className="gifs-container">
+            {
+                mockGifs.map((gif) => (
+                    <div key={gif.id} className="gif-card">
+                        <img src={gif.url} alt={gif.title} />
+                        <h3>{gif.title}</h3>
+                        <p>
+                            {gif.width}x{gif.height} (1.5mb )
+                        </p>
+                    </div>
+                ))
+            }
+        </div>
+    </>
+  );
+};
